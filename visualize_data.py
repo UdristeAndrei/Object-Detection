67c4b7f266cc5678e2143abf9686843e35a03779
UdristeAndrei <udriste.andrei.98@gmail.com>
2023-11-14 15:31:04 +0100

    Created two module, the first one is visualize_data.py, which is used to visualize the bounding box for different damages, to get a better understanding of how they look/work. The second module is called process_data, and has the purpose to modify the name of the images, such that they are easier to follow. On top of this it also generates the labels that will be used for training the model. The labels will be generated in a .txt file according to yolov5 standard.

---
+++ visualize_data.py
@@ -0,0 +1,26 @@
+import json
+import cv2
+import numpy as np
+
+file_path = "vehicle_damage_detection_dataset/vehicle_damage_detection_dataset/"
+
+
+def visualize_data(data_path, set_name="train", nr_images=5):
+    with open(data_path + f"annotations/instances_{set_name}.json") as f:
+        data = json.load(f)
+
+        for idx in range(len(data["annotations"]))[:nr_images]:
+            img_id = data["annotations"][idx]["image_id"]
+            b_box = np.round(data["annotations"][idx]["bbox"]).astype(int)
+            img_path = data_path + f"images/{set_name}/" + data["images"][img_id]["file_name"]
+
+            image = cv2.imread(img_path)
+
+            cv2.rectangle(image, (b_box[0], b_box[1]), (b_box[0] + b_box[2], b_box[1] + b_box[3]), (255, 0, 0))
+            cv2.imshow("test", image)
+
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+
+visualize_data(file_path)

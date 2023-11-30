Corrected a bug in the process_data.py and visualize_data.py modules, where the normalization of the images was done wrong. The initial assumption was that all the images have the same size, but that was modified. Also converted the labels from the coco_paper format into the coco2017 format used by Yolo.
Created the pre_trained.py module, which runs the inference of the data on an existing pretrained model, in this case it uses the YoloV8m model. The module runs the inference on the first 100 test images, and it can show the predicted bbox, that is in green, the annotations that are in blue, and the intersection between those, those are in red. The module also computes some basic statistics of the model like class accuracy, global accuracy, and the average iou value per class.

COCO/process_data.py
@@ -1,34 +1,57 @@
 import json
 import os
+import cv2
 import shutil
 import numpy as np
 
 file_path = "data/data2017/"
-img_height = 640
 
 
-def generate_labels(save_path, annotation_path, img_id):
+def normalize_bbox(bbox, img_height=640, img_width=640):
+    bbox[0] = bbox[0] / img_width
+    bbox[1] = bbox[1] / img_height
+    bbox[2] = bbox[2] / img_width
+    bbox[3] = bbox[3] / img_height
+    return bbox
+
+
+def convert_coco91_coco80(category_id):
+    missing_values = [12, 26, 29, 30, 45, 66, 68, 69, 71, 83, 91]
+
+    if category_id < 12:
+        return category_id - 1
+
+    for idx, missing_value in enumerate(missing_values):
+        if category_id < missing_value:
+            return category_id - idx - 1
+
+
+def generate_labels(save_path, annotation_path, img_id, h, w):
     with open(annotation_path) as f_json:
         annotations = json.load(f_json)
 
         with open(save_path + "/" + img_id + ".txt", "w") as f_txt:
             for annotation in annotations:
 
-                bbox = np.array(annotation["bbox"]) / img_height
+                class_id = convert_coco91_coco80(annotation["category_id"])
+                bbox = normalize_bbox(np.array(annotation["bbox"]), h, w)
+
                 transformed_bbox = [bbox[0] + bbox[2] / 2, bbox[1] + bbox[3] / 2, bbox[2], bbox[3]]
                 str_bbox = " ".join(str(value) for value in transformed_bbox)
 
-                f_txt.write(str(annotation["category_id"]) + " " + str_bbox + "\n")
+                f_txt.write(str(class_id) + " " + str_bbox + "\n")
 
 
 def create_dir(annotation_path, img_id, set_name="train"):
     os.makedirs("data/images/" + set_name, exist_ok=True)
     os.makedirs("data/labels/" + set_name, exist_ok=True)
 
-    generate_labels("data/labels/" + set_name, annotation_path, img_id)
-
     img_name = img_id + ".jpg"
+    image = cv2.imread(file_path + img_name)
+    h, w, c = image.shape
+
     shutil.copy(file_path + img_name, "data/images/" + set_name + "/" + img_name)
+    generate_labels("data/labels/" + set_name, annotation_path, img_id, h, w)
 
 
 def data_split(path, train_proc=0.7, val_proc=0.2):
@@ -47,5 +70,5 @@ def data_split(path, train_proc=0.7, val_proc=0.2):
         create_dir(path + file, file[:-5], "test")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     data_split(file_path)

COCO/visualize_data.py
@@ -3,28 +3,27 @@ import json
 import cv2
 import numpy as np
 
-img_height = 640
 imgs_path = "data/images/train/"
 annotations_path = "data/labels/train/"
 
 
-def transform_bbox(b_box):
-    x1 = int(round((b_box[0] - b_box[2]/2) * img_height, 0))
-    x2 = int(round((b_box[0] + b_box[2]/2) * img_height, 0))
+def transform_bbox(b_box, h, w):
+    x1 = int(round((b_box[0] - b_box[2]/2) * w, 0))
+    x2 = int(round((b_box[0] + b_box[2]/2) * w, 0))
 
-    y1 = int(round((b_box[1] - b_box[3]/2) * img_height, 0))
-    y2 = int(round((b_box[1] + b_box[3]/2) * img_height, 0))
+    y1 = int(round((b_box[1] - b_box[3]/2) * h, 0))
+    y2 = int(round((b_box[1] + b_box[3]/2) * h, 0))
 
     return x1, y1, x2, y2
 
 
-def gen_bboxes(image, annotation_path):
+def gen_bboxes(image, annotation_path, h, w):
 
     with open(annotation_path) as f:
         for line in f:
 
             line = [float(x) for x in line.split(" ")]
-            bbox = transform_bbox(line[1:])
+            bbox = transform_bbox(line[1:], h, w)
             cv2.rectangle(image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), np.random.uniform(0, 256, 3))
 
 
@@ -33,13 +32,15 @@ def visualize_data(data_path, nr_img=5):
         img_path = imgs_path + img_name
         annotation_path = annotations_path + img_name[:-4] + ".txt"
 
-
         image = cv2.imread(img_path)
-        gen_bboxes(image, annotation_path)
+        h, w, c = image.shape
+
+        gen_bboxes(image, annotation_path, h, w)
         cv2.imshow("test", image)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     visualize_data(imgs_path)